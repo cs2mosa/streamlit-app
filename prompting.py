8e1118191f39ba1cf0f0a1f850066bc7c3c68154
cs2mosa <mosaashinawy1@gmail.com>
2024-10-14 12:50:08 +0300

    Update prompting.py

--- prompting.py
+++ prompting.py
@@ -1,6 +1,6 @@
 import vertexai
 from vertexai.generative_models import GenerativeModel
-project_id = "innate-works-436112-e6"
+project_id = "your project name"
 region = "us-central1"
 vertexai.init(project= project_id, location= region)
 model = GenerativeModel("gemini-pro-vision")
